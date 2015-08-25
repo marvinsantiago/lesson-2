14e4ae8ccdc4e6000632ee47d88b258343da91b3
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-25 13:09:41 -0500

    Updated URL for forecast.io

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get('/*', function (req, res) {
   var path = req.originalUrl;
 
-  request('http://addb.absolutdrinks.com/' + path + '?apiKey=23311a636eb84606a031b1292272666b').pipe(res);
+  request('https://api.forecast.io/forecast/c4ec0d8427e0f79cd082031268378ba5/37.8267,-122.423' + path).pipe(res);
 });
 
 app.listen(process.env.PORT || 3000);
