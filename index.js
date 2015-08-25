b2a8d7de62c92d28e3081ecb7eda99e8d55af614
Marvin Santiago <marvin.santiago@ymail.com>
2015-08-25 13:41:47 -0500

    Updated URL

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ app.use(cors());
 app.get('/*', function (req, res) {
   var path = req.originalUrl;
 
-  request('https://api.forecast.io/forecast/c4ec0d8427e0f79cd082031268378ba5/37.8267,-122.423' + path).pipe(res);
+  request('https://api.forecast.io/forecast/c4ec0d8427e0f79cd082031268378ba5' + path).pipe(res);
 });
 
 app.listen(process.env.PORT || 3000);
